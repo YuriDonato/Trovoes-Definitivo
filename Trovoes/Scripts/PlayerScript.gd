10e032331144b719f7cbabfb43f11b60bb323e72
YuriDonato <yuridonatosis@gmail.com>
2022-11-06 13:22:55 -0300

    Base Tavern

--- Trovoes/Scripts/PlayerScript.gd
+++ Trovoes/Scripts/PlayerScript.gd
@@ -1,12 +1,19 @@
 extends KinematicBody2D
 
-export var speed := 700.0
+export var speed := 150.0
 
 func _physics_process(delta: float) -> void:
+	
+
+
 	var input_vector := Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
 	)
 	
 	var move_direction := input_vector.normalized()
 	move_and_slide(speed * move_direction)
+func _process(_delta: float) -> void:
+	if Input.action_press("Shift") == true: 
+		speed = 250.0
--- Trovoes/Scripts/PlayerTestFacingDirection_SpriteGD.gd
+++ Trovoes/Scripts/PlayerTestFacingDirection_SpriteGD.gd
@@ -1,5 +1,6 @@
 extends Sprite
 
+export var bounce_speed := 8.0
 
 const SPRITES_MAP :={
 	Vector2.LEFT: preload("C:/Users/User/Documents/GitHub/Trovoes-Definitivo/Trovoes/Assets/Sprite/PlayerTest/testildo_left.png"),
@@ -24,4 +25,4 @@ func _process(_delta: float) -> void:
 		texture = SPRITES_MAP[input_vector]
 		look_direction = input_vector
 		flip_h = sign(look_direction.x) == 1.0
-	position.y = sin(OS.get_ticks_msec() / 200.0) * 8.0
+	position.y = sin(OS.get_ticks_msec() / 200.0) * bounce_speed
